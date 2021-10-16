b2caa2abc5b9b68cbddf163db3f3d1a338668efe
Ezequiel Gonzalez <edgaregonzalez@gmail.com>
2021-10-16 14:21:01 -0300

    add phone numer to user model

--- backend/models/users.js
+++ backend/models/users.js
@@ -43,6 +43,10 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       type: DataTypes.STRING
     },
+    phone: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
     status: {
       allowNull: false,
       defaultValue: 1,
